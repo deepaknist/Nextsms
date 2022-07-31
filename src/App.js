628f639e3708faa76abc8bdee28d14c5ff681a31
Deepak kumar <deepak.code2436@gmail.com>
2022-07-31 23:00:46 +0530

    added name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Deepak
+          Learn React with Deepak mishra
         </a>
       </header>
     </div>
